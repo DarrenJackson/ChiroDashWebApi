86019adf206aa6278ecfeb714b840f743eba8096
Darren Jackson <darren.c.jackson@gmail.com>
2018-12-20 20:09:41 +0000

    Added Swagger

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ChiroDashWebApi
 {
@@ -15,6 +16,11 @@ namespace ChiroDashWebApi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSingleton<IDoctorServices, DoctorServices>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ChiroDash API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +36,12 @@ namespace ChiroDashWebApi
             }
 
             app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChiroDash API V1");
+            });
+
             app.UseMvc();
         }
     }
