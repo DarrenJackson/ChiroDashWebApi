486f3fa4d16976513296ffd74219ea1228cbbbe0
Darren Jackson <darren.c.jackson@gmail.com>
2019-05-12 23:35:39 +0100

    Enable CORS

--- ChiroDash.WebUI/Startup.cs
+++ ChiroDash.WebUI/Startup.cs
@@ -23,6 +23,11 @@ namespace ChiroDash.WebUI
             {
                 c.SwaggerDoc("v1", new Info { Title = "ChiroDash API", Version = "v1" });
             });
+
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
